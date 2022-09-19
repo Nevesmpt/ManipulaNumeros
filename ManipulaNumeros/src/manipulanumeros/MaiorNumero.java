6fe566f6f25d50202f7ca1eab003744c6d701df6
Manuel Neves <manuelmneves@hotmail.com>
2022-09-19 15:12:46 +0100

    maior

---
+++ ManipulaNumeros/src/manipulanumeros/MaiorNumero.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package manipulanumeros;
+
+
+public class MaiorNumero {
+
+    static void maior2(int x, int y) {
+        if(x>y)
+            System.out.println(x+"maior que"+y);
+        else if (y>x)
+            System.out.println(y+" maior que "+x);
+        else
+            System.out.println(x+" igual a "+y);
+                    
+                    
+        
+    }
+    
+}
---
+++ ManipulaNumeros/src/manipulanumeros/ManipulaNumeros.java
@@ -0,0 +1,40 @@
+
+package manipulanumeros;
+
+import java.util.Scanner;
+
+
+public class ManipulaNumeros {
+
+    public static Scanner ler = new Scanner(System.in);
+    public static int x, y;
+    public static void main(String[] args) {
+        
+        int op;
+        do{
+            System.out.println("Menu de operação com numeros");
+            System.out.println("1 - Maior de 2 numeros");
+            
+            System.out.println("0 - Sair");
+            System.out.println("Seleccione a opção pretendida");
+            op = ler.nextInt();
+            switch(op){
+                case 0: break;
+                case 1:
+                    lerValores("Insira um valor", "Insira outro valor");
+                    MaiorNumero.maior2(x,y);
+                    break;
+                default: System.out.println("Insira opção válida");
+            }
+            
+        }while(op!=0);
+    }
+
+    private static void lerValores(String s1, String s2) {
+        System.out.println(s1);
+        x = ler.nextInt();
+        System.out.println(s2);
+        y = ler.nextInt();
+    }
+    
+}
