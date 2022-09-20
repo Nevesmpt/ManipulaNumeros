e6b8ab4c6d30b70b5366196825aa96a94d59a569
Manuel Neves <manuelmneves@hotmail.com>
2022-09-20 16:49:33 +0100

    banco

--- ManipulaNumeros/src/manipulanumeros/Banco.java
+++ ManipulaNumeros/src/manipulanumeros/Banco.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package manipulanumeros;
 
-/**
- *
- * @author mneves
- */
+
 class Banco {
+
+    static void calculaCredito() {
+        
+        double n;
+        double c;
+        System.out.println("Insira o seu saldo médio");
+        n = ManipulaNumeros.ler.nextDouble(); //camel case
+        switch((int)n/200) { //switch so recebe inteiros ou caracteres
+            case 0:
+                //0-199
+                System.out.println("O seu saldo é "+n+"Não tem direito a crédito");    //nao existe credito        
+            break;
+            case 1:
+                //201-400
+                c = n*0.20;
+                System.out.println("O seu saldo é "+n+"O seu crédito é"+c);
+            break;
+            case 2:
+                //401-600
+                c = n*0.30;
+                System.out.println("O seu saldo é "+n+"O seu crédito é"+c);
+            break;          
+            default:
+                //+601
+                c = n*0.40;
+                System.out.println("O seu saldo é "+n+"O seu crédito é"+c);
     
+        
+    }
+    
+}
 }
